b0b6c37dcd70b4d6244916f3d961fb2d2c882559
Davhick123 <hicksd4504@student.faytechcc.edu>
2025-02-22 12:23:11 -0500

    Add files via upload

---
+++ P1LAB1_HicksDavid.py
@@ -0,0 +1,16 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+print("Try programiz.pro")# Your Name
+# Date
+# Assignment Name: Introduction to Python 3 Lab
+# A brief description of the project: This program prompts the user to enter their first name and last name, 
+# then displays a welcome message to the user.
+
+# Prompt the user for their first name
+first_name = input("Enter your first name: ")
+
+# Prompt the user for their last name
+last_name = input("Enter your last name: ")
+
+# Display the welcome message
+print("Hello, " + first_name + " " + last_name + "! Welcome to CTI-110")
